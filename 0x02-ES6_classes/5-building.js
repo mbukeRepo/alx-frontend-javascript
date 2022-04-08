4601c41ee9f49fd66693c98dd30c04f5fe1f145e
mbukeRepo <mbuke404@gmail.com>
2022-04-08 15:28:57 +0200

    linted 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,19 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined)
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
+
   get sqft() {
     return this._sqft;
   }
 
   set sqft(sqft) {
-    if (typeof(sqft) !== 'number')
+    if (typeof (sqft) !== 'number') {
       throw new TypeError('sqft must be a number');
+    }
     this._sqft = sqft;
   }
 }
