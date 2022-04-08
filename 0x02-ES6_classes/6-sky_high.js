51743563135f2aa5e7b0c4800a4bbc3fc14da902
mbukeRepo <mbuke404@gmail.com>
2022-04-08 15:31:27 +0200

    linted 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -5,15 +5,18 @@ export default class SkyHighBuilding extends Building {
     super(sqft);
     this.floors = floors;
   }
+
   get floors() {
     return this._floors;
   }
 
   set floors(floors) {
-    if (typeof(floors) !== 'number')
+    if (typeof (floors) !== 'number') {
       throw new TypeError('floors must be a number');
+    }
     this._floors = floors;
   }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this.floors} floors`;
   }
