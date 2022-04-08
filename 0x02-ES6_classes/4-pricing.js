a0d8af371858cbf681ad6185eb0a76ae992f9bf9
mbukeRepo <mbuke404@gmail.com>
2022-04-08 15:25:33 +0200

    linted and corrected 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,27 +1,38 @@
-import Currency from './3-currency'
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
     this.currency = currency;
   }
+
   get amount() {
     return this._amount;
   }
+
   get currency() {
     return this._currency;
   }
+
   set amount(amount) {
-    if (typeof(amount) !== 'number')
+    if (typeof (amount) !== 'number') {
       throw new TypeError('amount must be a number');
+    }
     this._amount = amount;
   }
+
   set currency(currency) {
-    if (!(currency instanceof Currency))
+    if (!(currency instanceof Currency)) {
       throw new TypeError('currency must be a Currency');
+    }
     this._currency = currency;
   }
+
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
   }
 }
