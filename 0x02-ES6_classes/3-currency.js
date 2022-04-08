bba1cf6f957eab4cf12d6430df9c2e8fcfa2be70
mbukeRepo <mbuke404@gmail.com>
2022-04-08 15:19:10 +0200

    linted 3-currency.js

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,22 +3,29 @@ export default class Currency {
     this.code = code;
     this.name = name;
   }
+
   get name() {
     return this._name;
   }
+
   set name(name) {
-    if (typeof(name) !== 'string')
+    if (typeof (name) !== 'string') {
       throw new TypeError('name must be a string');
+    }
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(code) {
-    if (typeof(code) !== 'string')
+    if (typeof (code) !== 'string') {
       throw new TypeError('code must be a string');
+    }
     this._code = code;
   }
+
   displayFullCurrency() {
     return `${this.name} (${this.code})`;
   }
